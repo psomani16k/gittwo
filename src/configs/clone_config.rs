9adfcc31dcf6bd6f2d4b9eac7aba7ddf024130a4
parth <psomani16k@gmail.com>
2025-05-09 17:37:39 +0530

    added documentation

--- src/configs/clone_config.rs
+++ src/configs/clone_config.rs
@@ -1,9 +1,10 @@
 use std::{
-    num::NonZeroUsize,
     path::{Path, PathBuf},
     sync::mpsc,
 };
 
+#[derive(Clone)]
+/// A struct used to specify various details needed to clone a repository.
 pub struct CloneConfig {
     pub(crate) clone_dir_name: String,
     pub(crate) parent_path: PathBuf,
@@ -15,6 +16,12 @@ pub struct CloneConfig {
 }
 
 impl CloneConfig {
+    /// Takes in a `url` to clone the repository from and
+    /// a `parent_dir` to clone the repository into.
+    ///
+    /// The clone process will make a directory with the
+    /// repository name in the `parent_dir` and clone the repository
+    /// into this dir, just like how git does.
     pub fn new(url: impl Into<String>, parent_dir: &Path) -> Self {
         let target_dir: String = url.into();
         let url = target_dir.clone();
@@ -36,28 +43,37 @@ impl CloneConfig {
     }
 
     // getters
+
+    /// Returns true if owner validation is to be skipped, false otherwise.
     pub fn get_skip_owner_validation(&self) -> bool {
         return self.skip_owner_validation;
     }
 
+    /// Returns the url set for the repository to be cloned.
     pub fn get_url(&self) -> &str {
         return &self.url;
     }
 
+    /// Returns true if certification checks are to be bypassed, false otherwise.
     pub fn get_bypass_certificate_check(&self) -> bool {
         return self.bypass_certificate_check;
     }
 
+    /// Returns the consumer (Receiver) end of a mpsc. Intended to receive
+    /// git cli like update messages with an associated index.
     pub fn get_update_channel(&mut self) -> mpsc::Receiver<(usize, String)> {
         let (sender, receiver) = mpsc::channel();
         self.sender = Some(sender);
         return receiver;
     }
 
+    /// Returns the directory where the repository is to be cloned.
     pub fn get_parent_path(&self) -> PathBuf {
         return self.parent_path.clone();
     }
 
+    /// Returns the name of the directory where the content of the repository will
+    /// be. The final path of the repository will be the parent_path/clone_dir_name/
     pub fn get_clone_dir_name(&self) -> String {
         if self.flags.bare {
             let mut dir = self.clone_dir_name.clone();
@@ -68,18 +84,25 @@ impl CloneConfig {
     }
 
     // setters
+
+    /// Set true to skip owner validation.
     pub fn skip_owner_validation(&mut self, skip: bool) {
         self.skip_owner_validation = skip;
     }
 
+    /// Set true to skip certification checks.
     pub fn bypass_certificate_check(&mut self, bypass: bool) {
         self.bypass_certificate_check = bypass;
     }
 
+    /// Set a custom name for the cloned repository. This will only change
+    /// the name of the directory that will be formed, parent path will still
+    /// be where the directory is created.
     pub fn custom_clone_directory(&mut self, dir: impl Into<String>) {
         self.clone_dir_name = dir.into();
     }
 
+    /// "Pass" a flag to the git clone command.
     pub fn add_flag(&mut self, flag: CloneFlags) {
         match flag {
             CloneFlags::Branch(branch) => self.flags.branch = branch,
@@ -90,6 +113,7 @@ impl CloneConfig {
     }
 }
 
+#[derive(Clone, PartialEq, PartialOrd)]
 pub(crate) struct CloneFlagsInternal {
     pub(crate) branch: Option<String>, // Branch(String),
     pub(crate) depth: Option<usize>,   // Depth(NonZeroUsize),
@@ -108,6 +132,7 @@ impl Default for CloneFlagsInternal {
     }
 }
 
+/// An enum representing the various flags that can be added to the `git clone` command.
 pub enum CloneFlags {
     /// `--branch` or `-b` flag for git clone.
     /// Some(branch) will set the flag, None will unset it.
--- src/repository/repository.rs
+++ src/repository/repository.rs
@@ -16,6 +16,7 @@ pub struct GitRepository {
 }
 
 impl GitRepository {
+    /// Create a `GitRepository` from an existing repository.
     pub fn open(path: &Path) -> Result<Self, Error> {
         let repo = Repository::open(path)?;
         return Ok(GitRepository {
@@ -25,6 +26,7 @@ impl GitRepository {
         });
     }
 
+    /// Create an empty `GitRepository` object. Use this for cloning a repository.
     pub fn new() -> Self {
         GitRepository {
             cred: GitCredentials::Default,
@@ -33,18 +35,27 @@ impl GitRepository {
         }
     }
 
+    /// Set credentials of the type username and password. Used when interacting
+    /// with a remote repository over HTTPS.
     pub fn set_user_pass(&mut self, user: impl Into<String>, pass: impl Into<String>) {
         let http_cred = GitHttpsCredentials::new(Some(user.into()), Some(pass.into()));
         self.cred = GitCredentials::Https(http_cred);
     }
 
+    /// Set credentials of the type username. Used when interacting
+    /// with a remote repository over HTTPS.
     pub fn set_user(&mut self, user: impl Into<String>) {
         let http_cred = GitHttpsCredentials::new(Some(user.into()), None);
         self.cred = GitCredentials::Https(http_cred);
     }
 
+    /// Performs an action equivalent to `git clone`. Returns `Ok(())` if the repository
+    /// is already cloned. If not, clones the repository and makes `self` ready for other
+    /// operations on the repository.
     pub fn git_clone(&mut self, config: CloneConfig) -> Result<(), Error> {
-        let mut config = config;
+        if self.repository.is_some() {
+            return Ok(());
+        }
         let mut fetch_options = FetchOptions::new();
         let mut repo_builder = RepoBuilder::new();
         let mut callbacks = RemoteCallbacks::new();
